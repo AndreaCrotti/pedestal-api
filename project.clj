ca3187cda29919171735f2c3dc14b6cae991a28e
Andrea Crotti <andrea.crotti.0@gmail.com>
2021-04-13 10:11:28 +0100

    use other route swagger

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject pedestal-api "0.3.6-SNAPSHOT"
+(defproject andreacrotti/pedestal-api "0.3.7"
   :description "A batteries-included API for Pedestal using Swagger"
   :url "https://github.com/oliyh/pedestal-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[oliyh/route-swagger "0.1.6"]]
+  :dependencies [[andreacrotti/route-swagger "0.1.7"]]
   :repl-options {:init-ns user}
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]
                                        [io.pedestal/pedestal.service "0.5.8"]]}
